5b4de39afb3119ffaeebb89e20c3a593c2004577
tivler <thomasdmckyer@gmail.com>
2018-08-08 23:21:46 -0400

    deleted everything and starting over

--- js/controllers/GlobalController.js
+++
@@ -1,9 +0,0 @@
-app.controller('GlobalCtrl', ['$scope', function ($scope) {
-    console.log('GlobalCtrl loaded');
-
-    $scope.infoView = function () {
-        console.log('Going to inf land');
-        window.location.href = './views/infa.html';
-    }
-
-}]);
