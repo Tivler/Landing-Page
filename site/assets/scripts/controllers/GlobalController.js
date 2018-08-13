ae19d94512fadba06a5b566c46d730cc07f0b964
tivler <thomasdmckyer@gmail.com>
2018-08-13 12:02:09 -0400

    Redid the entire site added video header and nav bar, positioned logo and created sections

--- site/assets/scripts/controllers/GlobalController.js
+++
@@ -1,9 +0,0 @@
-app.controller('GlobalCtrl', ['$scope', function ($scope) {
-    console.log('GlobalCtrl loaded');
-
-    $scope.resumeView = function () {
-        console.log('Going to resume land');
-        window.location.href = '../views/resume.html';
-    }
-    
-}]);
