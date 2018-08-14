e6adf39f562e9a0f4ac942f41e2ea0dd2aa9f9bd
tivler <thomasdmckyer@gmail.com>
2018-08-14 14:25:01 -0400

    added my image paths to display on site and added my about me section to my site

---
+++ site.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    const whatIDo = document.querySelector('.what-i-do');
+    
+        typer(whatIDo, 80)
+        .pause(100)
+        .line('Creative Thinker')
+        .pause(1250)
+        .back('all' , 50)
+        .continue('Highly Motivated')
+        .pause(1250)
+        .back('all' , 50)
+        .continue('Self-Taught')
+        .pause(1250)
+        .back('all' , 50)
+        .continue('The Life of Tivler')
+        .pause(1250)
+        .back('all' , 50)
+        .continue('A Full-Stack Software Engineer...')
+});
+
