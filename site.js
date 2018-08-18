c567d17005c6bb8b6d8eda068b40f8d1ce1b32b6
tivler <thomasdmckyer@gmail.com>
2018-08-18 16:19:18 -0400

    Made Icons responsive and targeted them to link to specific websites

--- site.js
+++ site.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+(function () {
     const whatIDo = document.querySelector('.what-i-do');
-    
-        typer(whatIDo, 80)
+
+    typer(whatIDo, 80)
         .pause(100)
         .line('Creative Thinker')
         .pause(1250)
-        .back('all' , 50)
+        .back('all', 50)
         .continue('Highly Motivated')
         .pause(1250)
-        .back('all' , 50)
+        .back('all', 50)
         .continue('Self-Taught')
         .pause(1250)
-        .back('all' , 50)
+        .back('all', 50)
         .continue('The Life of Tivler')
         .pause(1250)
-        .back('all' , 50)
+        .back('all', 50)
         .continue('A Full-Stack Software Engineer...')
-});
-
+})();
