b6aaca61efc04c51e7f858d1edf7737c571c3caf
tivler <thomasdmckyer@gmail.com>
2019-01-28 18:17:33 -0500

    added sub heading to my site

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 (function () {
-    const whatIDo = document.querySelector('.me-hero');
+    const whatIDo = document.querySelector('.me-subhero');
 
     typer(whatIDo, 150)
-        .pause(150)
+        .pause(175)
         .line('Thomas McKyer...')
 })();
 
-const hero = document.querySelector('.me-hero')
+const hero = document.querySelector('.main_hero')
 hero.classList.add('animated', 'bounceInLeft');
